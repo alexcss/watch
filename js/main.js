54c2733ff058c38be21c8d7178ad79fb766d2bf9
AlexCSS <kalyuzhnyi@gmail.com>
2016-12-30 12:32:54 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-;(function(){
+;(function($){
 
-})();
+	$(function(){
+		var slider = $('.ba-slider');
+
+		slider.slick({
+			dots: true,
+			vertical: true,
+			verticalSwiping: true,
+			infinite: false
+		});
+
+		slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+			var currentSlideEl = $(slick.$slides[currentSlide]),
+				nextSlideEl = $(slick.$slides[nextSlide]),
+
+				currentClass = currentSlideEl.data('watch'),
+				nextClass = nextSlideEl.data('watch');
+
+				$('body').removeClass(currentClass).addClass(nextClass);
+
+		});
+
+	});
+
+})(jQuery);
